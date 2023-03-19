03763ed0faed80fc0d14a67e87a83b427d9b6004
ukizet <ukikatuki@gmail.com>
2023-03-19 21:36:06 +0200

    2

--- handlers/admin.py
+++ handlers/admin.py
@@ -12,64 +12,59 @@ class FSMAdmin(StatesGroup):
     desc = State()
     price = State()
 
-async def statements(func: str) -> function:
-    # global cm_start, load_photo, load_name, load_desc, load_price
-
-    # тут я спробую зробити шаблон всіх цих функцій, так як весь цей код не відповідає DRY
-    async def load_template(message: types.Message, state: FSMContext, load_type: str, text: str='', finish: bool=False):
-        if load_type == 'photo':
-            async with state.proxy() as data:
-                data['photo'] = message.photo[0].file_id
-        else:
-            async with state.proxy() as data:
-                data[f'{load_type}'] = message.text
-        if finish == True:
-            async with state.proxy() as data:
-                await message.answer(str(data), reply_markup=client_kb)
-
-            await sqlite_db.sql_add(state=state)
-            await state.finish()
-        else:
-            await FSMAdmin.next()
-            if len(text) > 0:
-                await message.answer(f'{text}')
-            else:
-                pass
-    
-    # Початок діалога завантаження нового елемента меню
-    async def cm_start(message : types.Message):
-        await FSMAdmin.photo.set()
-        await message.answer('Give me photo(стискай зображення)', reply_markup=cancel_button)
-
-    # Отримуємо першу відповідь від користувача та записуємо її в словник
-    async def load_photo(message : types.Message, state : FSMContext):
-        await load_template(message=message, state=state, load_type='photo', text='Give me name of the pizza')
-
-    # Отримуємо другу відповідь від користувача 
-    async def load_name(message : types.Message, state : FSMContext):
-        await load_template(message=message, state=state, load_type='name', text='Give me pizza description')
-
-    # Отримуємо третю відповідь від користувача 
-    async def load_desc(message : types.Message, state : FSMContext):
-        await load_template(message=message, state=state, load_type='desc', text='Give me price of the pizza')
-
-    # Отримуємо останню відповідь від користувача 
-    async def load_price(message : types.Message, state : FSMContext, get=None):
-        # if get == 'state': return FSMAdmin.price
-        await load_template(message=message, state=state, load_type='price', text='', finish=True)
-
-    return locals()[func]
+# Початок діалога завантаження нового елемента меню
+async def cm_start(message : types.Message):
+    await FSMAdmin.photo.set()
+    await message.answer('Give me photo(стискай зображення)', reply_markup=cancel_button)
 
 async def return_back_button(message : types.Message):
     await message.answer(reply_markup=client_kb, text='back')
     
 # Функція відміни
-async def cancel_handler_button(message : types.Message, state : FSMContext):
+async def cancel_handler(message : types.Message, state : FSMContext):
     current_state = await state.get_state()
     if current_state is None:
         return
     await state.finish()
     await message.reply('OK', reply_markup=client_kb)
+
+# тут я спробую зробити шаблон всіх цих функцій, так як весь цей код не відповідає DRY
+async def load_template(message: types.Message, state: FSMContext, load_type: str, text: str='', finish: bool=False):
+    if load_type == 'photo':
+        async with state.proxy() as data:
+            data['photo'] = message.photo[0].file_id
+    else:
+        async with state.proxy() as data:
+            data[f'{load_type}'] = message.text
+    if finish == True:
+        async with state.proxy() as data:
+            await message.answer(str(data), reply_markup=client_kb)
+
+        await sqlite_db.sql_add(state=state)
+        await state.finish()
+    else:
+        await FSMAdmin.next()
+        if len(text) > 0:
+            await message.answer(f'{text}')
+        else:
+            pass
+
+# Отримуємо першу відповідь від користувача та записуємо її в словник
+async def load_photo(message : types.Message, state : FSMContext):
+    await load_template(message=message, state=state, load_type='photo', text='Give me name of the pizza')
+
+# Отримуємо другу відповідь від користувача 
+async def load_name(message : types.Message, state : FSMContext):
+    await load_template(message=message, state=state, load_type='name', text='Give me pizza description')
+
+# Отримуємо третю відповідь від користувача 
+async def load_desc(message : types.Message, state : FSMContext):
+    await load_template(message=message, state=state, load_type='desc', text='Give me price of the pizza')
+
+# Отримуємо останню відповідь від користувача 
+async def load_price(message : types.Message, state : FSMContext, get=None):
+    # if get == 'state': return FSMAdmin.price
+    await load_template(message=message, state=state, load_type='price', text='', finish=True)
 
 def register_handlers_admin(dp : Dispatcher):
     """
@@ -78,16 +73,16 @@ def register_handlers_admin(dp : Dispatcher):
     можна зберігати в самих функціях і потім їх якось підтягувати в цикл при реєстрації
     """
     def register_buttons():
-        dp.register_message_handler(statements('cm_start')(), Text(equals='Завантажити піцу'), state=None)
+        dp.register_message_handler(cm_start, Text(equals='Завантажити піцу'), state=None)
         dp.register_message_handler(return_back_button, Text(equals='Повернутися назад'))
-        dp.register_message_handler(cancel_handler_button, Text(equals='Відміна'), state="*")
+        dp.register_message_handler(cancel_handler, Text(equals='Відміна'), state="*")
 
     def register_cm_start_states():
-        dp.register_message_handler(statements('load_photo')(), content_types=['photo'], state=FSMAdmin.photo)
-        dp.register_message_handler(statements('load_name')(), state=FSMAdmin.name)
-        dp.register_message_handler(statements('load_desc')(), state=FSMAdmin.desc)
-        dp.register_message_handler(statements('load_price')(), state=FSMAdmin.price)
-        # dp.register_message_handler(cancel_handler_button, state=)
+        dp.register_message_handler(load_photo, content_types=['photo'], state=FSMAdmin.photo)
+        dp.register_message_handler(load_name, state=FSMAdmin.name)
+        dp.register_message_handler(load_desc, state=FSMAdmin.desc)
+        dp.register_message_handler(load_price, state=FSMAdmin.price)
+        # dp.register_message_handler(cancel_handler, state=)
 
     register_buttons()
     register_cm_start_states()
